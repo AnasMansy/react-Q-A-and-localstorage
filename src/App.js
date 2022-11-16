06279584e16fd5fb925d67e8dbbef72bcc14f23a
Anas Mansy <anas393238@gmail.com>
2022-11-16 17:01:08 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import { Navbar, Nav, Col, Button, Container, Row } from 'react-bootstrap';
 import React, { Component, useState } from 'react'
-import { person } from './data';
-import FormIn from './component/FormIn';
-import QList from './component/QList';
-import { qustions } from './data';
+import NavBar from './component/NavBar';
+import Header from './component/Header';
+import Category from './component/Category';
+import Items from './component/Items';
+import menu from './data';
 function App() {
-  const [data,setData]=useState(qustions);
- 
-  const storageData=JSON.parse(localStorage.getItem("items")) 
-  const addItem=()=>{
-    localStorage.setItem("items",JSON.stringify([...qustions]))
-    setData([...qustions])
+  const [items, setItems] = useState(menu)
+  const uniqueCat = ["all", ...new Set(menu.map(item => item.cat))];
+  const filterCategory = (e) => {
+    if (e === "all")
+      setItems(menu)
+    else {
+      const newItems = menu.filter(item => item.cat == e)
+      setItems(newItems)
+    }
   }
-  const deleteAll=()=>{
-    if(localStorage.getItem("items")!=null){
-    qustions.splice(0,qustions.length)
-    localStorage.removeItem("items")}
-    setData([...qustions])
-  }
-  const deleteOne=()=>{
-    if(localStorage.getItem("items")!=null){
-    setData([...qustions])
-    localStorage.setItem("items",JSON.stringify([...qustions]))}
+
+  const search = (e) => {
+      const newItems = menu.filter(item => item.title.includes(e)  )
+      setItems(newItems)
   }
   return (
     <div className="App">
-      <Container className='py-5'>
-        <Row className='justify-content-center'> 
-        <Col sm="4" className="  text-center p-3 h4">
-            Q & A
-            </Col>  
-          <Col sm="8" className=" text-center">
-            <FormIn onAdd={addItem}/>
-            <QList data={data} deleteOne={deleteOne}/>
-            <Row className='px-2'>
-            {storageData?(<button className='btn btn-danger   mt-3  'onClick={deleteAll}>delete all</button>): null}  
-            </Row>
-          </Col>
-        </Row>
-        
+      <NavBar search={search} />
+      <Container className=''>
+        <Header />
+        <Category filterCategory={filterCategory} menu={uniqueCat} />
+        <Items items={items} />
       </Container>
     </div>
   );
